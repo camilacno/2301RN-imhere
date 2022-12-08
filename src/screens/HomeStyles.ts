29d6f060b5ebb1f36f55eb2e58c3d8e2539a8c7d
Camila Nepomuceno <camila.cno@gmail.com>
2022-12-08 20:23:12 -0300

    Initial comit + home page

---
+++ src/screens/HomeStyles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+    padding: 24,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    borderRadius: 5,
+    color: '#fff',
+    height: 56,
+    padding: 16,
+    fontSize: 16,
+  },
+  button: {},
+})
